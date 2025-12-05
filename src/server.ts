d7cba9a7277965b9486f70713d83d44ecc790ad7
Wahedul Islam <wahedulislam.pro@gmail.com>
2025-12-05 12:40:48 +0600

    setup file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,34 @@
-import express, { Request, Response } from "express";
+import express, { json, Request, Response } from "express";
+import {Pool} from "pg"
 
 const app = express();
+app.use(express.json());
+
+const pool = new Pool({
+    connectionString : 'postgresql://neondb_owner:npg_LNYOn6ZDC8of@ep-billowing-union-a86c1sly-pooler.eastus2.azure.neon.tech/neondb?sslmode=require&channel_binding=require'
+})
+
+const initDB = async ()=>{
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL,
+        phone TEXT NOT NULL,
+        role TEXT NOT NULL CHECK (role IN ('admin','customer')),
+        created_at TIMESTAMP DEFAULT NOW()
+        );
+    `)
+    console.log("Database Connented");
+}
+
+initDB();
+
+app.post("/users", async (req : Request, res : Response)=>{
+    const body = req.body;
+    console.log(body);
+})
 
 app.get("/",(req : Request, res : Response)=>{
     res.status(200).json({
